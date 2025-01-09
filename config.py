e40c2512f4d5de4bb9db0805d8c81ceebce9adef
FAITHPAPAD <waji.dfakeyt@gmail.com>
2025-01-10 00:17:14 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7550987469:AAEjXOHykyun6sv4YhNF2lLTz2NknF-G15U")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@FAKEYT700")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6830887977")
+
+
+
+
+
+
+
+  
